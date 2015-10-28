507214be5d9ca49f0ba5a45080fada81fa36fd01
Federico Pomar <fmpomar@gmail.com>
2015-10-28 02:14:29 -0300

    Multiples cambios, utilizando NetworkX, Dijkstra funcionando, Graficos con matplotlib a minutos de realizarse

--- main.py
+++ main.py
@@ -1,159 +1,166 @@
+import networkx as nx
 import simpy
-from math import *
-
 from collections import deque
 from random import expovariate, normalvariate, choice, seed
-
-
-var_lambda = 0.1
-muA = 1
-sigmaA = 0.1
-muB = 2
-sigmaB = 0.2
-
-destination_random_vars = {
-    'A' : { 'B' : 0.1 },
-    'B' : { 'A' : 0.05 }
-}
-
-# Matriz de tiempo de viaje sin carga
-# contains: (mean, std deviation)
-service_times = {
-    'A' : { 'B' : (7, 1) },
-    'B' : { 'A' : (30, 3) }
-}
+from matplotlib import pyplot as plt
 
 SIMULATION_TIME = 100
 
-class Node():
-    #constructor
-    def __init__(self, environment, identifier):
-        self.id = identifier
-        self.env = environment
-        self.arcs = {}
-        self.routing_table = {'B' : 'B', 'A' : 'A'}
 
-    def add_arc(self, arc, node):
-        assert node.id not in self.arcs
-        self.arcs[node.id] = arc
+def exponential_var_gen(var_lambda):
+    while True:
+        yield int(expovariate(var_lambda))
 
-    def trigger(self):
-        while env.now < SIMULATION_TIME:
-            msg = self._generate_msg()
-            var_lambda = destination_random_vars[self.id][msg.destination]
-            time_between_arrivals = expovariate(var_lambda)
-            yield self.env.timeout(time_between_arrivals)
+
+def normal_var_gen(var_mu, var_sigma):
+    while True:
+        yield int(normalvariate(var_mu, var_sigma))
+
+
+class MessageRouter(object):
+    def __init__(self, node, queues):
+        self.queues = queues
+        self.node = node
+
+    def set_route(self, target, queue):
+        self.queues[target] = queue
+
+    def route(self, msg):
+        if msg.destination == self.node:
+            print('DESTROY', msg, ' in ', self.node, ' at ', msg.env.now)
+            return
+        next_queue = self.queues.get(msg.destination, None)
+        if next_queue is None:
+            raise Exception("Se cago todo error")
+        print('ROUTE', msg, ' from ', self.node, ' to ', next_queue.next_router.node, ' at ', msg.env.now)
+        next_queue.enqueue(msg)
+
+
+class MessageQueue(object):
+    def __init__(self, env, next_router, service_times):
+        self.next_router = next_router
+        self.queue = deque([])
+        self.env = env
+        self.service_times = service_times
+
+    def enqueue(self, msg):
+        self.queue.append(msg)
+        if len(self.queue) == 1:
+            self.env.process(self._process_next())
+
+    def _process_next(self):
+        while len(self.queue) > 0:
+            service_time = next(self.service_times)
+            yield self.env.timeout(service_time)
+            msg = self.queue.popleft()
+            print('TRANSIT', msg, ' to ', self.next_router.node, ' at ', self.env.now, ' in ', service_time)
+            self.next_router.route(msg)
+
+
+class MessageSpawner(object):
+    def __init__(self, env, origin, origin_router, demand):
+        self.env = env
+        self.origin = origin
+        self.origin_router = origin_router
+        self.demand = demand
+
+    def initialize(self):
+        for destination, spawn_times in self.demand.items():
+            simpy.events.Process(env, self._trigger(destination, spawn_times))
+
+    def _trigger(self, destination, spawn_times):
+        while self.env.now < SIMULATION_TIME:
+            yield self.env.timeout(next(spawn_times))
+            msg = self._generate_msg(destination)
             msg.init_timestamp()
-            self._send_msg(msg)
-
-    def receive_msg(self, msg):
-        if msg.destination == self.id:
-            params = (self.id, msg.origin, self.env.now, msg.time_in_transit())
-            print("%s: Received msg from %s at %f, took %f to arrive" % params)
-        else:
+            print('CREATE', msg)
             self._send_msg(msg)
 
     def _send_msg(self, msg):
-        self._update_follow_up_node(msg)
-        arc = self._choose_arc(msg.follow_up_node)
-        arc.queue_msg(msg)
+        self.origin_router.route(msg)
 
-    def _choose_arc(self, follow_up_node):
-        return self.arcs[follow_up_node]
-
-    def _generate_msg(self):
-        network_nodes = tuple(destination_random_vars[self.id].keys())
-        destination = choice(network_nodes)
-        return Message(self.env, self.id, destination)
-
-    def _update_follow_up_node(self, msg):
-        follow_up_node = self.routing_table[msg.destination]
-        msg.update_follow_up_node(follow_up_node)
+    def _generate_msg(self, destination):
+        return Message(self.env, self.origin, destination)
 
 
-class Arc():
-    def __init__(self, environment, source_node, target_node, weight):
-        self.env = environment
-        self.source = source_node
-        self.target = target_node
-        self.weight = weight
-
-        # returns queue for each destination
-        bck_id = bck_node.id
-        fwd_id = fwd_node.id
-        self.queues = {
-            bck_id : Queue(*service_times[fwd_id][bck_id]),
-            fwd_id : Queue(*service_times[bck_id][fwd_id])
-        }
-
-    def queue_msg(self, msg):
-        queue = self._get_queue(msg)
-        queue.append(msg)
-        if len(queue) == 1:
-            env.process(self._process_queue_element(queue))
-
-    def _get_queue(self, msg):
-        return self.queues[msg.follow_up_node]
-
-    def _process_queue_element(self, queue):
-        while len(queue) > 0:
-            service_time, msg = queue.popleft()
-            msg.update_timestamp(service_time)
-            yield self.env.timeout(service_time)
-            self._deliver_msg(msg)
-
-    def _deliver_msg(self, msg):
-        if msg.follow_up_node == self.fwd_node.id:
-            self.fwd_node.receive_msg(msg)
-        else:
-            self.bck_node.receive_msg(msg)
-
-
-class Message():
+class Message(object):
     def __init__(self, environment, origin, destination):
         self.env = environment
         self.origin_timestamp = None
         self.timestamp = None
         self.origin = origin
         self.destination = destination
-        self.follow_up_node = None
-
-    def update_timestamp(self, time):
-        assert self.timestamp is not None
-        self.timestamp += time
 
     def init_timestamp(self):
         self.timestamp = self.origin_timestamp = self.env.now
 
     def time_in_transit(self):
-        return self.timestamp - self.origin_timestamp
+        return self.timestamp - self.env.now
 
-    def update_follow_up_node(self, node_id):
-        self.follow_up_node = node_id
-
-
-class Queue(deque):
-    def __init__(self, serv_time_mean, serv_time_dev):
-        super().__init__()
-        self.mean = serv_time_mean
-        self.dev = serv_time_dev
-
-    def popleft(self):
-        service_time = normalvariate(self.mean, self.dev)
-        return service_time, super().popleft()
+    def __str__(self):
+        return '<Message (' + str(self.origin) + \
+               ' -> ' + str(self.destination) + \
+               ') created at ' + str(self.timestamp) + \
+               '>'
 
 
-def generate_graph(env):
-    A = Node(env, 'A')
-    B = Node(env, 'B')
-    arc = Arc(env, A, B)
-    A.add_arc(arc, B)
-    B.add_arc(arc, A)
-    return A, B
+class NetworkGraph(nx.DiGraph):
+    def __init__(self, env, data = None, **attr):
+        super(NetworkGraph, self).__init__(data, **attr)
+        self.env = env
+
+    def add_network_node(self, name, demand):
+        demand = dict(map(lambda kv: (kv[0], exponential_var_gen(kv[1])), demand.items()))
+        router = MessageRouter(name, {})
+        spawner = MessageSpawner(self.env, name, router, demand)
+        self.add_node(name, router=router, spawner=spawner)
+
+    def add_network_edge(self, source, destination, mu, sigma):
+        destination_router = self.node[destination]['router']
+        queue = MessageQueue(self.env, destination_router, normal_var_gen(mu, sigma))
+        self.add_edge(source, destination, queue=queue, mu=mu, sigma=sigma)
+
+    def update_routing(self, weight):
+        results = nx.shortest_path(self, weight=weight)
+        for node, paths in results.items():
+            for target, path in paths.items():
+                if len(path) > 1:
+                    router = self.node[node]['router']
+                    next_node = path[1]
+                    queue = self.edge[node][next_node]['queue']
+                    router.set_route(target, queue)
+                    print(node, target, path)
+
+    def initialize_spawners(self):
+        for node, attributes in self.node.items():
+            attributes['spawner'].initialize()
+
+
+def create_graph(env):
+    new_graph = NetworkGraph(env)
+    new_graph.add_network_node('A', {'B': 1.0/100})
+    new_graph.add_network_node('B', {'A': 1.0/100})
+    new_graph.add_network_node('C', {})
+    new_graph.add_network_edge('A', 'B', 50, 5)
+    new_graph.add_network_edge('B', 'A', 50, 5)
+    # new_graph.add_network_edge('A', 'C', 4, 1)
+    # new_graph.add_network_edge('C', 'A', 4, 1)
+    # new_graph.add_network_edge('B', 'C', 4, 1)
+    # new_graph.add_network_edge('C', 'B', 4, 1)
+    return new_graph
+
+def print_routing_status(graph):
+    for node, attr in graph.node.items():
+        for target, queue in attr['router'].queues.items():
+            print(node, target, queue.next_router.node)
+
 
 if __name__ == '__main__':
     seed(42)
     env = simpy.Environment()
-    nodes = generate_graph(env)
-    processes = [simpy.events.Process(env, node.trigger()) for node in nodes]
+    graph = create_graph(env)
+    graph.update_routing("mu")
+    print_routing_status(graph)
+    print(graph.node.items())
+    graph.initialize_spawners()
     env.run()
